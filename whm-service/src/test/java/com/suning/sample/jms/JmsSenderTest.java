a1356f12980a877396f2965bb72dbb67a7f90980
dodoer <dodoer@78c6e549-bab0-4dcd-97f5-38ca0bb93b42>
2016-07-30 11:20:44 +0000

    Initial import.

---
+++ whm-service/src/test/java/com/suning/sample/jms/JmsSenderTest.java
@@ -0,0 +1,14 @@
+//package com.suning.sample.jms;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.testng.annotations.Test;
+//
+//
+//public class JmsSenderTest extends BaseTest{
+//    @Autowired SampleSender sender;
+//    int i = 0;
+//    @Test(invocationCount=10)
+//    public void testSend(){
+//        
+//        sender.simpleSend("Test Message" + i++);
+//    }
+//}
