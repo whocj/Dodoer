4028dc7a9cfcc0d0d5dfd0203b6a748d9c8e27bd
dodoer <dodoer@78c6e549-bab0-4dcd-97f5-38ca0bb93b42>
2016-08-14 02:19:17 +0000

    小说

---
+++ whm-spider/src/test/java/com/suning/sample/TestPattern.java
@@ -0,0 +1,17 @@
+package com.suning.sample;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestPattern {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+           String s = "“是！”应声依然整齐。←百度搜索→【ㄨ书?阅ぁ屋www.ShuYueWu.Com】整齐。←百度搜索→【ㄨ书?阅ぁ屋www.ShuYueWu.Com】三垣四象再成阵势，虽威势不再，仍然让刚刚经历了生死";
+           // Pattern p = Pattern.compile("(\\[[0-9]*\\])") ; //提取出来 [770]
+           Pattern p = Pattern.compile("(?<=【).*(?=】)"); // /我想直接提取出来 770 不能包括 [1_1]中的 两个1
+           Matcher m = p.matcher(s);
+           while (m.find()) {
+               System.out.println(s.replace(m.group(), ""));
+           }
+       }
+}
