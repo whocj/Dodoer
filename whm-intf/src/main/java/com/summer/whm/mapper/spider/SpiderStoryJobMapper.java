b31ab94bcfa561a83e8f157a64f4845431dad2ca
dodoer <dodoer@78c6e549-bab0-4dcd-97f5-38ca0bb93b42>
2016-08-18 14:45:59 +0000

    爬虫

--- whm-intf/src/main/java/com/summer/whm/mapper/spider/SpiderStoryJobMapper.java
+++ whm-intf/src/main/java/com/summer/whm/mapper/spider/SpiderStoryJobMapper.java
@@ -1,7 +1,15 @@
 package com.summer.whm.mapper.spider;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.summer.whm.entiry.spider.SpiderStoryJob;
 import com.summer.whm.mapper.BaseMapper;
 
 public interface SpiderStoryJobMapper extends BaseMapper {
+    List<SpiderStoryJob> queryByTempateIdAndStatus(@Param("templateId") Integer templateId,
+            @Param("status") String status);
 
+    SpiderStoryJob queryByUrl(@Param("url") String url);
 }
